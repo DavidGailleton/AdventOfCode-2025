ea1cd0450069c57f22d60554a1391e93d878a3be
David Gailleton <dgaillet@z4r9p4.42lyon.fr>
2025-12-08 12:56:39 +0100

    03 Bonus done

--- 03_b/main.c
+++ 03_b/main.c
@@ -6,7 +6,7 @@
 /*   By: dgaillet <dgaillet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/07 12:53:07 by dgaillet          #+#    #+#             */
-/*   Updated: 2025/12/08 11:08:38 by dgaillet         ###   ########lyon.fr   */
+/*   Updated: 2025/12/08 12:55:55 by dgaillet         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,43 @@
 #include "get_next_char.h"
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
-// to low 11928
-// to low 16427
-// To low 16449
+char	*get_highest_nbr(char *base, size_t remain, char *actual_best, char *result)
+{
+	// printf("Result = %s", result);
+	if (remain == 0)
+		return (result);
+	if ((strlen(base)) <= remain)
+		return (actual_best);
+	if (base[0] > actual_best[0])
+		actual_best = base;
+	actual_best = get_highest_nbr(base + 1, remain, actual_best, result);
+	// printf("%s\n", actual_best);
+	if (actual_best == base)
+		return (get_highest_nbr(base + 1, remain - 1, base + 1, ft_strjoin_new(result, actual_best, 0)));
+	return (actual_best);
+}
 
 int	main(void)
 {
 	char		*temp;
+	char		*highest_temp;
 	int			fd;
-	char		*first = NULL;
-	char		*second = NULL;
-	int			i;
-	long long	result = 0;
+	unsigned long long	result = 0;
 
 	fd = open("input", O_RDONLY);
 	temp = get_next_char(fd, '\n');
 	while (temp)
 	{
-		printf ("Actual comb : %s\n", temp);
-		i = 0;
-		first = temp;
-		while (temp[i + 2])
-		{
-			if (temp[i] > first[0])
-				first = &temp[i];
-			i++;
-		}
-		i = 1;
-		second = first + 1;
-		while (first[i + 1])
-		{
-			if (first[i] > second[0])
-				second = &first[i];
-			i++;
-		}
-		printf("Best comb = %c%c\n\n", first[0], second[0]);
-		result += (first[0] - '0') * 10;
-		result += second[0] - '0';
+		printf ("Actual comb : %s", temp);
+		highest_temp = get_highest_nbr(temp, 12, temp, ft_strdup(""));
+		printf("highest Comb : %s\n\n", highest_temp);
+		result += atoll(highest_temp);
+		free(highest_temp);
 		free(temp);
 		temp = get_next_char(fd, '\n');
 	}
 	close(fd);
-	printf("%lld\n", result);
+	printf("Result : %llu\n", result);
 }
